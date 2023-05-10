ce32abb87c521730737b51d540177fe6c52c1c2f
Leaf The Witch <120463199+LeafTheWitch@users.noreply.github.com>
2023-05-10 09:25:25 +0200

    created placeholder already sent screen

---
+++ lib/quizparts/AlreadySentScreen.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class AlreadySentScreen extends StatelessWidget {
+  const AlreadySentScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
