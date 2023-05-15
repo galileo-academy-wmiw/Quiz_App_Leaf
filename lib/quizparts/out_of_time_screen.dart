2b8b3c31b7a2bdcb1522e62be5bb3e5e4f377250
Leaf The Witch <120463199+LeafTheWitch@users.noreply.github.com>
2023-05-15 14:09:01 +0200

    made out of time screen look nice

--- lib/quizparts/out_of_time_screen.dart
+++ lib/quizparts/out_of_time_screen.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:quiz_app/library.dart' as lib;
 
 class OutOfTimeScreen extends StatelessWidget {
   const OutOfTimeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-        ],
-      ),
+        color: Colors.white,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FaIcon(
+                FontAwesomeIcons.hippo,
+                size: (screenWidth / 6) * 5,
+                color: Colors.amber,
+              ),
+              SizedBox(
+                width: (screenWidth / 6) * 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "sorry",
+                      style: lib.headingText,
+                      textScaleFactor: 4,
+                    ),
+                    Text(
+                      "The Time Hippo has decided you're out of time...",
+                      style: lib.baseText,
+                      textScaleFactor: 1.25,
+                    ),
+                    Text(
+                      "No worries tho, there's always next time",
+                      style: lib.baseText,
+                      textScaleFactor: 1.25,
+                    ),
+                  ],
+                ),
+              )
+            ]
+        )
     );
   }
 }
