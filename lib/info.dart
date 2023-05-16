2f6ad6de2043740dd61e162e9469a54aaa9a2b18
Leaf The Witch <120463199+LeafTheWitch@users.noreply.github.com>
2023-05-16 12:49:01 +0200

    made the text more fun and added DRAGONS🐉

--- lib/info.dart
+++ lib/info.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:quiz_app/library.dart' as lib;
 
 class Info extends StatelessWidget {
@@ -11,22 +11,37 @@ class Info extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     return Container(
       color: Colors.white,
-      child: const Padding(
+      child: Padding(
         padding: EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("About This App", style: lib.headingText, textScaleFactor: 2,),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text("This app is a quiz app. your scores are stored in our beautiful database, and the person who gets the most answers right wins exactly one nothing. you can only play once though, so do your best to win absolutely nothing",
-                style: lib.baseText,
-                textScaleFactor: 1.25,
-              ),
-            )
+            FaIcon(
+              FontAwesomeIcons.dragon,
+              size: (screenWidth / 6) * 4,
+              color: Colors.green,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "About This App",
+                  style: lib.headingText,
+                  textScaleFactor: 2,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "The quiz dragon has cometh to ask thee to answer these questions three.... or more if we add more questions. You only get one chance though, so good luck and may the quiz gods be with you",
+                    style: lib.baseText,
+                    textScaleFactor: 1.25,
+                  ),
+                )
+              ],
+            ),
           ],
-
         ),
       ),
     );
