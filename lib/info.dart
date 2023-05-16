53d7336ea49dc1daf2d86235e0b0ecfec8e40d49
Leaf The Witch <120463199+LeafTheWitch@users.noreply.github.com>
2023-05-16 12:33:50 +0200

    made a simple info screen

--- lib/info.dart
+++ lib/info.dart
@@ -7,10 +7,27 @@ class Info extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Container(
       color: Colors.white,
-      child: Column(
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("About This App", style: lib.headingText, textScaleFactor: 2,),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text("This app is a quiz app. your scores are stored in our beautiful database, and the person who gets the most answers right wins exactly one nothing. you can only play once though, so do your best to win absolutely nothing",
+                style: lib.baseText,
+                textScaleFactor: 1.25,
+              ),
+            )
+          ],
 
+        ),
       ),
     );
   }
